243ab7a040de95fc3886c007c923b90d44080576
sudosteak <sudosteak@github.com>
2025-10-03 15:09:32 -0400

    finished lab3 or lab4 (i forget) script for configuring the firewall-cmd shit

--- linux/fwd_rules.sh
+++ linux/fwd_rules.sh
@@ -5,18 +5,27 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
-
-#flush all existing rules
-firewall-cmd --permanent --remove
-
+# define variables
 service=ssh
 accept_ip="172.16.31.0/16"
 deny_ip="172.16.32.0/16"
+host_ip="172.16.0.1/16"
 
-# allow
-firewall-cmd --permanent --zone=public --add-service=$service --source=$accept_ip
+# flush all existing ssh rules for these sources
+firewall-cmd --permanent --zone=public --remove-rich-rule="rule family=\"ipv4\" source address=\"$accept_ip\" service name=\"$service\" accept" 2>/dev/null
+firewall-cmd --permanent --zone=public --remove-rich-rule="rule family=\"ipv4\" source address=\"$deny_ip\" service name=\"$service\" reject" 2>/dev/null
 
-# block (remove the service for that source)
-firewall-cmd --permanent --zone=public --remove-service=$service --source=$deny_ip
+# allow ssh from accept_ip
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family=\"ipv4\" source address=\"$accept_ip\" service name=\"$service\" accept"
+
+# block ssh from deny_ip
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family=\"ipv4\" source address=\"$deny_ip\" service name=\"$service\" reject"
+
+# accept ssh from host pc
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family=\"ipv4\" source address=\"$host_ip\" service name=\"$service\" accept"  
 
 firewall-cmd --reload
+
+# display current rules
+echo "current firewall rules:"
+firewall-cmd --list-all
