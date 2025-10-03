ba2b81512dd37c96eb744be9d080fa7602cdecd8
sudosteak <sudosteak@github.com>
2025-10-03 15:18:04 -0400

    changed variable names and rules that can be flushed

--- linux/fwd_rules.sh
+++ linux/fwd_rules.sh
@@ -7,16 +7,17 @@ fi
 
 # define variables
 service=ssh
-accept_ip="172.16.31.0/16"
-deny_ip="172.16.32.0/16"
+client_ip="172.16.31.0/16"
 host_ip="172.16.0.1/16"
 
 # flush all existing ssh rules for these sources
-firewall-cmd --permanent --zone=public --remove-rich-rule="rule family=\"ipv4\" source address=\"$accept_ip\" service name=\"$service\" accept" 2>/dev/null
-firewall-cmd --permanent --zone=public --remove-rich-rule="rule family=\"ipv4\" source address=\"$deny_ip\" service name=\"$service\" reject" 2>/dev/null
+firewall-cmd --permanent --zone=public --remove-rich-rule="rule family=\"ipv4\" source address=\"$client_ip\" service name=\"$service\" accept" 2>/dev/null
+firewall-cmd --permanent --zone=public --remove-rich-rule="rule family=\"ipv4\" source address=\"$host_ip\" service name=\"$service\" accept" 2>/dev/null
+firewall-cmd --permanent --zone=public --remove-rich-rule="rule family=\"ipv4\" source address=\"0.0.0.0/0\" service name=\"$service\" reject" 2>/dev/null
 
-# allow ssh from accept_ip and host_ip
-firewall-cmd --permanent --zone=public --add-rich-rule="rule family=\"ipv4\" source address=\"$accept_ip\" service name=\"$service\" accept"
+
+# allow ssh from client_ip and host_ip
+firewall-cmd --permanent --zone=public --add-rich-rule="rule family=\"ipv4\" source address=\"$client_ip\" service name=\"$service\" accept"
 firewall-cmd --permanent --zone=public --add-rich-rule="rule family=\"ipv4\" source address=\"$host_ip\" service name=\"$service\" accept"
 
 # block ssh from everyone else
