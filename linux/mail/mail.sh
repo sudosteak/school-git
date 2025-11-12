ca11a67e4c0b9973aabe1c4cd5b14c58af53af58
sudosteak <devel9943@proton.me>
2025-11-12 18:05:43 -0500

    fix: handle errors in postfix and aliases configuration commands

--- linux/mail/mail.sh
+++ linux/mail/mail.sh
@@ -27,7 +27,7 @@ cp -a /etc/postfix/main.cf{,.bak.$(date +%s)} 2>/dev/null || true
 # configure postfix
 # check if already configured
 echo "configuring postfix"
-cat $HOME/school-git/linux/mail/postfix_main > /etc/postfix/main.cf
+cat $HOME/school-git/linux/mail/postfix_main > /etc/postfix/main.cf || true
 
 # enable and start postfix
 systemctl restart postfix
@@ -67,7 +67,7 @@ systemctl restart named
 cp -a /etc/aliases{,.bak.$(date +%s)} 2>/dev/null || true
 
 # add aliases to /etc/aliases
-cat $HOME/school-git/linux/mail/aliases > /etc/aliases
+cat $HOME/school-git/linux/mail/aliases > /etc/aliases || true
 newaliases
 postalias /etc/aliases
 postalias -q geeks /etc/aliases
@@ -75,7 +75,14 @@ postalias -q geeks /etc/aliases
 # restart postfix to apply changes
 systemctl restart postfix
 
+# configure iptables
+# flush existing rules
+iptables -F
+
 # allow client and server networks to access mail server on port 25 and reject alias network
 iptables -I INPUT -p tcp --dport 25 -s "172.16.30.0/24" -j ACCEPT
 iptables -I INPUT -p tcp --dport 25 -s "172.16.31.0/24" -j ACCEPT
 iptables -I INPUT -p tcp --dport 25 -s "172.16.32.0/24" -j REJECT
+
+# show iptables rules
+iptables --list
